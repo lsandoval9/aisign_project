48f27d6044e43c4c725159bb7de3bdce7b4ebdd8
lsandoval9 <lizarraga309@gmail.com>
2025-05-06 10:20:31 -0400

    feat: create model for app users

---
+++ lib/core/auth/models/user_model.dart
@@ -0,0 +1,24 @@
+import 'package:equatable/equatable.dart';
+
+class UserModel extends Equatable {
+  final String id;
+  final String email;
+  final String name;
+
+  const UserModel({
+    required this.id,
+    required this.email,
+    required this.name,
+  });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      id: json['id'] as String,
+      email: json['email'] as String,
+      name: json['name'] as String,
+    );
+  }
+
+  @override
+  List<Object?> get props => [id, email, name];
+}
